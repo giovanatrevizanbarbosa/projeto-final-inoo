048bf83879d759e9ae348e10a298eb7bde86943d
Giovana Trevizan Barbosa <gi.trevizan.barbosa@gmail.com>
2023-12-08 17:09:32 -0300

    alteracoes no menu utilizando JOptionPane

--- src/br/edu/ifsp/arq/tsi/inoo/view/Menu.java
+++ src/br/edu/ifsp/arq/tsi/inoo/view/Menu.java
@@ -19,18 +19,59 @@ public class Menu {
 
             switch (option) {
                 case 0:
-                    String name = JOptionPane.showInputDialog("Digite o nome do cliente:");
-                    String document = JOptionPane.showInputDialog("Digite o CPF ou CNPJ do cliente:");
-                    confirm = JOptionPane.showConfirmDialog(null, "\nDeseja confirmar o cadastro?" 
-                                                                + "\nCliente inserido:\nNome: " + name 
-                                                                + "\nCPF/CNPJ: " + document, "Cadastro de Cliente", JOptionPane.YES_NO_OPTION);
+                    Object[] personType = { "Pessoa Física", "Pessoa Jurídica", "Cancelar" };
 
-                    Client client = new ClientNaturalPerson(name, document);
-                    if(confirm == 0 && clientController.save(client)){
-                        JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
-                    }else{
-                        JOptionPane.showMessageDialog(null, "Cadastro cancelado");
-                    }
+                    int result = JOptionPane.showOptionDialog(
+                            null,
+                            "Informe o tipo de cliente",
+                            "",
+                            JOptionPane.YES_NO_CANCEL_OPTION,
+                            JOptionPane.QUESTION_MESSAGE,
+                            null,
+                            personType,
+                            personType[0]);
+
+                    // Analisando a escolha do usuário
+                    switch (result) {
+                        case JOptionPane.YES_OPTION:
+                            String name = JOptionPane.showInputDialog("Digite o nome do cliente:");
+                            String document = JOptionPane.showInputDialog("Digite o CPF do cliente:");
+                            confirm = JOptionPane.showConfirmDialog(null, "\nDeseja confirmar o cadastro?"
+                                    + "\nCliente inserido:\nNome: " + name
+                                    + "\nCPF: " + document, "Cadastro de Cliente", JOptionPane.YES_NO_OPTION);
+
+                            Client client = new ClientNaturalPerson(name, document);
+                            if (confirm == 0 && clientController.save(client)) {
+                                JOptionPane.showMessageDialog(null,
+                                        "Cadastro realizado com sucesso!");
+                            } else {
+                                JOptionPane.showMessageDialog(null, "Cadastro cancelado");
+                            }
+                            break;
+                        case JOptionPane.NO_OPTION:
+                            name = JOptionPane.showInputDialog("Digite o nome do cliente:");
+                            document = JOptionPane.showInputDialog("Digite o CNPJ do cliente:");
+                            String corporateReason = JOptionPane.showInputDialog("Digite a razão social do cliente:");
+                            confirm = JOptionPane.showConfirmDialog(null, "\nDeseja confirmar o cadastro?"
+                                    + "\nCliente inserido:\nNome: " + name
+                                    + "\nCNPJ: " + document
+                                    + "\n", "Cadastro de Cliente", JOptionPane.YES_NO_OPTION);
+                            client = new ClientJuridicalPerson(name, document, corporateReason);
+                            if (confirm == 0 && clientController.save(client)) {
+                                JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
+                            } else {
+                                JOptionPane.showMessageDialog(null,
+                                        "Cadastro cancelado");
+                            }
+                            break;
+                        case JOptionPane.CANCEL_OPTION:
+                            break;
+                        case JOptionPane.CLOSED_OPTION:
+                            break;
+                        default:
+                            System.out.println("Escolha inválida");
+                            break;
+                        }
                     break;
                 case 1:
                     String model = JOptionPane.showInputDialog("Digite o modelo do carro");
@@ -40,19 +81,19 @@ public class Menu {
                     int numberDoors = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de portas do carro"));
                     boolean hasAirConditioning = JOptionPane.showInputDialog("O carro possui ar condicionado?").equalsIgnoreCase("Sim") ? true : false;
                     double dailyRate = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor da diária do carro"));
-
+                    
                     Car car = new Car(model, brand, year, plate, numberDoors, hasAirConditioning, dailyRate);
-
+                    
                     String hasAir = hasAirConditioning ? "Possui" : "Não possui";
                     confirm = JOptionPane.showConfirmDialog(null, "\nDeseja confirmar o cadastro?"
-                                                                + "\nModelo: " + model 
-                                                                + "\nMarca: " + brand 
-                                                                + "\nAno: " + year 
-                                                                + "\nPlaca: " + plate 
-                                                                + "\nNúmero de portas: " + numberDoors 
-                                                                + "\nAr condicionado: " + hasAir
-                                                                + "\nValor da diária: " + dailyRate, "Confirmação do cadastro", JOptionPane.YES_NO_OPTION);
-
+                                                                                        + "\nModelo: " + model 
+                                                                                        + "\nMarca: " + brand 
+                                                                                        + "\nAno: " + year 
+                                                                                        + "\nPlaca: " + plate 
+                                                                                        + "\nNúmero de portas: " + numberDoors 
+                                                                                        + "\nAr condicionado: " + hasAir
+                                                                                        + "\nValor da diária: " + dailyRate, "Confirmação do cadastro", JOptionPane.YES_NO_OPTION);
+                        
                     if (carController.save(car) && confirm == 0) {
                         JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
                     } else {
@@ -60,20 +101,24 @@ public class Menu {
                     }
                     break;
                 case 2:
-                    int clientCode = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do cliente"));
-                    int carCode = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do carro"));
-                    int numberDiaries = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de diárias"));
-
-                    Rental rental = new Rental(numberDiaries, clientController.findById(clientCode), carController.findByCode(carCode));
-
-                    if(rentalController.rental(rental)){
-                        JOptionPane.showMessageDialog(null, "Carro alugado com sucesso!");
+                    if(clientController.getTotalClients() > 0 && carController.getTotalCars() > 0){
+                        int clientCode = Integer.parseInt(JOptionPane.showInputDialog(clientController.generateReport() + "\nDigite o código do cliente: "));
+                        int carCode = Integer.parseInt(JOptionPane.showInputDialog(carController.generateReport() + "\nDigite o código do carro: "));
+                        int numberDiaries = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de diárias"));
+    
+                        Rental rental = new Rental(numberDiaries, clientController.findById(clientCode), carController.findByCode(carCode));
+    
+                        if(rentalController.rental(rental)){
+                            JOptionPane.showMessageDialog(null, "Carro alugado com sucesso!");
+                        }else{
+                            JOptionPane.showMessageDialog(null, "Erro ao alugar carro!");
+                        }
                     }else{
-                        JOptionPane.showMessageDialog(null, "Erro ao alugar carro!");
+                        JOptionPane.showMessageDialog(null, "Não há clientes ou carros cadastrados!");
                     }
                     break;
                 case 3:
-                    carCode = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do carro"));
+                    int carCode = Integer.parseInt(JOptionPane.showInputDialog(carController.generateReport() + "\nDigite o código do carro"));
 
                     if(rentalController.devolution(rentalController.findByCarCode(carCode))){
                         JOptionPane.showMessageDialog(null, "Carro devolvido com sucesso!");
@@ -82,7 +127,7 @@ public class Menu {
                     }
                     break;
                 case 4:
-                    String report = rentalController.generateReport();
+                    String report = clientController.generateReport() + carController.generateReport() + rentalController.generateReport();
 
                     JOptionPane.showMessageDialog(null, report);
                     break;
